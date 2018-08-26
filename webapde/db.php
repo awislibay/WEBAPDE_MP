2f6ea76d8fde6587793d77d49c2ff7d6097cf511
JDNBaladjay <smoltortle@gmail.com>
2018-08-26 14:34:48 +0800

    fixed

--- webapde/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-	// Enter your Host, username, password, database below.
-	$con = mysqli_connect("localhost","root","","webapde");
-	// Check connection
-	if (mysqli_connect_errno())
-	{
-		echo "Failed to connect to Database: " . mysqli_connect_error();
-	}
-?>
