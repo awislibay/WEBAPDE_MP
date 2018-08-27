9705e2cc9765926cbde9b33dfc1afd78d3b54c3a
JDNBaladjay <smoltortle@gmail.com>
2018-08-27 13:49:02 +0800

    update

--- webapdeMP/webapde.sql
+++ webapdeMP/webapde.sql
@@ -1,1 +1,255 @@
-SELECT * FROM webapde.food;
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 27, 2018 at 07:47 AM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webapde`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food`
+--
+
+CREATE TABLE `food` (
+  `foodID` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `food`
+--
+
+INSERT INTO `food` (`foodID`, `name`, `price`) VALUES
+(21, 'Chocolate', 60),
+(22, 'Cucumber', 10),
+(23, 'Omelette Du Fromage', 100),
+(24, 'Spaghet', 30);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `inventoryID` int(11) NOT NULL,
+  `foodID` int(11) NOT NULL,
+  `toyID` int(11) NOT NULL,
+  `playerID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pet`
+--
+
+CREATE TABLE `pet` (
+  `petID` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `sprite` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pet`
+--
+
+INSERT INTO `pet` (`petID`, `name`, `sprite`) VALUES
+(11, 'Flying Cat', 'catbird.png'),
+(12, 'Kiwi', 'Kiwi.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player`
+--
+
+CREATE TABLE `player` (
+  `userID` int(11) NOT NULL,
+  `pet1ID` int(11) NOT NULL,
+  `pet2ID` int(11) NOT NULL,
+  `inventoryID` int(11) NOT NULL,
+  `moneyID` int(11) NOT NULL,
+  `profileID` int(11) NOT NULL,
+  `img` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `player`
+--
+
+INSERT INTO `player` (`userID`, `pet1ID`, `pet2ID`, `inventoryID`, `moneyID`, `profileID`, `img`) VALUES
+(1, 0, 0, 0, 0, 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `store`
+--
+
+CREATE TABLE `store` (
+  `storeID` int(11) NOT NULL,
+  `foodID` int(11) NOT NULL,
+  `toyID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `toy`
+--
+
+CREATE TABLE `toy` (
+  `toyID` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `toy`
+--
+
+INSERT INTO `toy` (`toyID`, `name`, `price`) VALUES
+(31, 'Ball', 50),
+(32, 'Stuffed Toy', 80),
+(33, 'Yarn', 20),
+(34, 'Fake bone', 20);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userID` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `birthday` date NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userID`, `username`, `birthday`, `password`) VALUES
+(1, 'aywis', '1998-12-29', 'password'),
+(2, 'helb', '2000-02-29', 'helb'),
+(3, 'rockstar69', '1999-12-29', 'password'),
+(4, 'jems', '1998-02-12', 'jems');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `food`
+--
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`foodID`);
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`inventoryID`);
+
+--
+-- Indexes for table `pet`
+--
+ALTER TABLE `pet`
+  ADD PRIMARY KEY (`petID`);
+
+--
+-- Indexes for table `player`
+--
+ALTER TABLE `player`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- Indexes for table `store`
+--
+ALTER TABLE `store`
+  ADD PRIMARY KEY (`storeID`);
+
+--
+-- Indexes for table `toy`
+--
+ALTER TABLE `toy`
+  ADD PRIMARY KEY (`toyID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `food`
+--
+ALTER TABLE `food`
+  MODIFY `foodID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `inventoryID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pet`
+--
+ALTER TABLE `pet`
+  MODIFY `petID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `player`
+--
+ALTER TABLE `player`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `store`
+--
+ALTER TABLE `store`
+  MODIFY `storeID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `toy`
+--
+ALTER TABLE `toy`
+  MODIFY `toyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
